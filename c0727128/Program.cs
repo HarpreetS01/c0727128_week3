2292ece297d76f935c2a1354d30a26b213b12f80
harpreet <happykamboj910@gmail.com>
2019-01-25 21:38:31 -0500

    Final

--- c0727128/Program.cs
+++ c0727128/Program.cs
@@ -13,16 +13,50 @@ namespace c0727128
         }
     }
 
-    class Countryside { }
+    class Countryside
+    {
+        Village Alst;
+        Village Maeland;
+        Village Schenig;
+
+        public void InitializeMap()
+        {
+            Village Alst = new Village();
+            Alst.VillageName = "Alst";
+            Village Maeland = new Village();
+            Maeland.VillageName = "Maeland";
+            Maeland.isAstrildeHere = true;
+            Village Schenig = new Village();
+            Schenig.VillageName = "Schenig";
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+
+        }
+
+        public void FindAstrilde()
+        {
+            Village CurrentVillage = this.Maeland;            
+            if (CurrentVillage.isAstrildeHere)
+            {
+                Console.WriteLine("hugi found Astrilde!!! celebrations!XOXO");
+            }
+
+            Village NextVillage;
+
+            NextVillage = CurrentVillage.NextVillage;
+        }
+    }
     
 
     class Village
 
     {
-        Village PreviousNode;
-        Village NextNode;
-        String VillageName;
-        bool isAstrildeHere = false;
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
         public Village() { }
     }
     
